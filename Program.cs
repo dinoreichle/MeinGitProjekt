1cae7120ad590cf3a942ba33e7ad08038a2256eb
Dino Reichle <dino_reichle@sluz.ch>
2026-09-15 11:30:54 +0200

    Übung W5 Aufgabe 4

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
-﻿Console.WriteLine("Ich heisse C#, Wie heisst du?");
-String Name =Console.ReadLine();
-Console.WriteLine($"Hallo {Name}, Schön bist du hier!");
+﻿decimal geld = 20.00m;
+decimal preisRiegel = 3.20m;
+
+decimal riegel = Math.Floor (geld / preisRiegel);
+Console.WriteLine($"Mit deinem Geld kannst du {riegel} riegel kaufen.");
+
+decimal restgeld = geld % preisRiegel;
+Console.WriteLine($"Du hast noch {restgeld} Fr. übrig.");
